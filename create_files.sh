a039bd246ca757f3ecfef6e9323420080113542b
Adriano Melo Fil <58274270+amelofilho@users.noreply.github.com>
2025-01-19 15:11:12 -0800

    Wrote create_files.sh

---
+++ create_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Script to create two files and write specific content to them
+
+# Create and write to test.txt
+echo "This is test.txt" > test.txt
+
+# Create and write to test2.txt
+echo "This is test2.txt" > test2.txt
+
+# Confirmation message
+echo "Files test.txt and test2.txt have been created and populated."
